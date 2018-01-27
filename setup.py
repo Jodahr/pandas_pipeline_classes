c4763384c5d4f1305fef3a21e25aaf49e8be11e9
Marcel <marcelrothering@fastmail.fm>
2018-01-27 15:40:51 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pandas_pipeline_classes',
-      version='0.1',
+      version='0.2',
       description='pipeline classes which return pandas DataFrames',
       url='http://githubcom/jodahr/pandas_pipeline_classes',
       author='jodahr',
       author_email='marcelrothering@fastmail.fm',
       licence='MIT',
       packages=['pandas_pipeline_classes'],
+      install_requires = ['pandas', 'sklearn', 'numpy']
       zip_safe=False)
